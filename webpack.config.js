634440c97b2b85e9607acbd31df5f381261d1da9
Evgeny Samsonov <samsonov@aeroidea.ru>
2021-02-01 21:33:57 +0300

    webpack-template with react v1 done

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, "tsconfig.json"),
+            },
+          },
+        ],
+        exclude: [path.resolve(__dirname, "node_modules")],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./www/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
+};
