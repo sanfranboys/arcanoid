17af4bcb738e72a09b8bdb8121e134f698649f5c
Aleksey_58 <lexa9108@inbox.ru>
2021-02-07 21:11:19 +0300

    [task/3/-registration]:fix styles

--- src/components/ContetntBox/ContentBox.tsx
+++ src/components/ContetntBox/ContentBox.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react'
 import './ContentBox.scss'
 
 const ContentBox: FC = ({ children }) => (
-  <div className="form-component">
-    <div className="form-component__wrapper">{children}</div>
+  <div className="content-box">
+    <div className="content-box__wrapper">{children}</div>
   </div>
 )
 
