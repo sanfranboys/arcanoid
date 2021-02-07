5cbe3826b6e37721b2b9626a4768989a10ae2ead
Aleksey <61862733+Domion-M@users.noreply.github.com>
2021-02-07 14:22:59 +0300

    Task/2/ auth (#4)
    * [task/2/-auth]:add page auth and style
    
    * fix
    
    * folder page
    
    * [task/2/-auth]-finish create page auth
    
    * [task/2/-auth]-add react-hook-form
    
    * [task/2/-auth]-realize r-h-f and refactor styles
    
    * [task/2/-auth]-fix
    
    * [task/2/-auth]:add htmlFor
    
    * [task/2/-auth]:add button and input component
    
    * [task/2/-auth]:refactor

---
+++ src/pages/AuthPage/components/Auth/Auth.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react'
+import { Form } from 'antd'
+import { useForm } from 'react-hook-form'
+import { AuthFormData } from '../../types'
+import Button from '../../../../components/Button'
+import Input from '../../../../components/Input'
+import './Auth.scss'
+
+const Auth: FC = () => {
+  const { handleSubmit, errors, register, setValue } = useForm<AuthFormData>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+  })
+
+  const onSubmit = (data: FormData) => console.log(data)
+
+  return (
+    <div className="auth">
+      <div className="auth__wrapper">
+        <Form className="auth__container" onFinish={handleSubmit(onSubmit)}>
+          <Input
+            label="Имя"
+            name="login"
+            id="login"
+            className="auth__container__input"
+            onChange={(e) => setValue('login', e.target.value)}
+            register={register(
+              { name: 'login' },
+              { required: true, minLength: 3 }
+            )}
+            error={!!errors.login}
+          />
+          <Input
+            label="Пароль"
+            name="password"
+            id="password"
+            className="auth__container__input"
+            onChange={(e) => setValue('password', e.target.value)}
+            register={register(
+              { name: 'password' },
+              { required: true, minLength: 6 }
+            )}
+            error={!!errors.password}
+          />
+          <div className="auth__wrapper-button">
+            <Button type="submit" className="auth__button">
+              Авторизоваться
+            </Button>
+          </div>
+        </Form>
+        <a href="/" className="auth__link">
+          Нет аккаунта?
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
