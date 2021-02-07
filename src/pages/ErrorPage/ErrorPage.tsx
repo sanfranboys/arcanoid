1e3a67400b59ea01068ca4720c822ba525f56439
Evgeny Samsonov <samsonov@aeroidea.ru>
2021-02-07 08:58:14 +0300

    added useMemo to ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 import Row from '../../components/Row'
 import Col from '../../components/Col'
 import Page from '../Page'
@@ -9,16 +9,22 @@ import getErrorTexts from './utils/getErrorTexts'
 const ErrorPage: FC<ErrorPageProps> = ({ errorType }) => {
   const { title, description } = getErrorTexts(errorType)
   const hasLink: boolean = errorType === 404
+
+  const errorBody = useMemo(
+    () => (
+      <Error
+        description={description}
+        hasLink={hasLink}
+        errorType={errorType}
+      />
+    ),
+    [errorType]
+  )
+
   return (
     <Page title={title}>
       <Row>
-        <Col span={24}>
-          <Error
-            description={description}
-            hasLink={hasLink}
-            errorType={errorType}
-          />
-        </Col>
+        <Col span={24}>{errorBody}</Col>
       </Row>
     </Page>
   )
