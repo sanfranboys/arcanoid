e3f17b2ee8224e1df138cb11472642f9a5460f02
Aleksey_58 <lexa9108@inbox.ru>
2021-02-12 09:41:55 +0300

    [task/27/-avatar]:refactor constant URL

--- src/elements/Avatar/Avatar.tsx
+++ src/elements/Avatar/Avatar.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react'
 import { Avatar as AntdAvatar, AvatarProps } from 'antd'
+import { BASE_URL } from '@/constants'
 import 'antd/lib/avatar/style/css'
 
 const Avatar: FC<AvatarProps> = ({ src, ...prop }) => (
   <AntdAvatar
-    src={src ? `https://ya-praktikum.tech${src}` : '/assets/images/avatar.png'}
+    src={src ? BASE_URL + src : '/assets/images/avatar.png'}
     {...prop}
   />
 )
