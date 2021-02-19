4798ede013ec977412187bea211c7d3ef64cfe94
Evgeny Samsonov <samsonov@aeroidea.ru>
2021-02-19 21:22:17 +0300

    sprint/2/13/-hoc: make forum as it was

--- src/pages/ForumPage/ForumPage.tsx
+++ src/pages/ForumPage/ForumPage.tsx
@@ -1,51 +1,16 @@
-import React, { useCallback, useMemo } from 'react'
-import { useHistory, useRouteMatch } from 'react-router-dom'
-import { Row, Col, Space } from '@/elements/'
-import { Description } from '@/components/'
+import React from 'react'
+import { Row, Col } from '@/elements/'
 import { Page } from '@/pages/'
-import Forum from './components/Forum'
-import { mock } from './mock'
+import { Forums } from './components/Forums'
 
-const ForumPage = () => {
-  const history = useHistory()
-  const { url } = useRouteMatch()
-
-  const goToTopic = useCallback(
-    (id: number) => () => history.push(`${url}/${id}`),
-    [history, url]
-  )
-
-  const forumList = useMemo(
-    () =>
-      mock.map((forum) => {
-        const { id } = forum
-        return <Forum key={id} forum={forum} onClick={goToTopic(id)} />
-      }),
-    [goToTopic]
-  )
-
-  return (
-    <Page title="Форум">
-      <Row>
-        <Col span={18} offset={4}>
-          <Space direction="vertical" full>
-            <Row gutter={[16, 16]}>
-              <Col span={20}>
-                <Description title="Форумы" />
-              </Col>
-              <Col span={2}>
-                <Description title="Темы" />
-              </Col>
-              <Col span={2}>
-                <Description title="Ответы" />
-              </Col>
-            </Row>
-            {forumList}
-          </Space>
-        </Col>
-      </Row>
-    </Page>
-  )
-}
+const ForumPage = () => (
+  <Page title="Форум">
+    <Row>
+      <Col span={18} offset={4}>
+        <Forums />
+      </Col>
+    </Row>
+  </Page>
+)
 
 export default ForumPage
