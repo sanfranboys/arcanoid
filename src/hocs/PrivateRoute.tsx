734f6e7db7a88cc2fa773659517fde284cda6140
Evgeny Samsonov <samsonov@aeroidea.ru>
2021-02-23 07:42:54 +0300

    sprint2: Revert "sprint2: hotfix"
    This reverts commit fc504061fd8dd0e2a10c7a063547381ff85d7439.

--- src/hocs/PrivateRoute.tsx
+++ src/hocs/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { getStorage } from '@/utils/'
 
 type GuardedRouteProps = {
   component: FC<any>
@@ -8,7 +9,7 @@ type GuardedRouteProps = {
 const PrivateRoute: FC<GuardedRouteProps> = ({ component: Component }) => {
   // заглушка, эти данные будут браться из стейта или локалсторейджа.
 
-  const isAuth: boolean = true
+  const isAuth: boolean = getStorage()
 
   return (
     <Route
