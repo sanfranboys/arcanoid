212135d18360c202b9c53d35de643910158157f7
Alexey Andrienko <aleksey.andrienko@rubius.com>
2021-02-26 00:23:13 +0700

    [sprint/2/10]: fix some bugs

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sanfracache'
+const CACHE_NAME = 'v1'
 const URLS = [
   '/',
   '/bundle.js',
@@ -9,45 +9,61 @@ const URLS = [
   '/assets/images/dog.jpg',
   '/assets/images/notbad.jpg',
   '/assets/images/error.jpg',
-
   '/assets/fonts/81d3c5139883841834aa2ac459370dc2.woff2',
   '/assets/fonts/98bad1928614d31281c78c122e584861.woff',
   '/assets/fonts/NotoSans.woff',
   '/assets/fonts/NotoSans.woff2',
 ]
 
+const WHITE_lIST = URLS.concat(['https://ya-praktikum.tech'])
+
 this.addEventListener('install', (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
       .then((cache) => cache.addAll(URLS))
-      .catch((err) => {
-        console.log(err)
+      .catch((error) => {
+        throw error
       })
   )
 })
 
 this.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      if (resp) {
-        return resp
-      }
-
+    caches.match(event.request).then((fromCache) => {
       const fetchRequest = event.request.clone()
+      const responseUrl = new URL(fetchRequest.url)
 
-      return fetch(fetchRequest).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response
-        }
+      return new Promise((resolve) => {
+        fetch(fetchRequest)
+          .then((response) => {
+            if (
+              !response ||
+              response.status !== 200 ||
+              !WHITE_lIST.some(
+                (url) =>
+                  responseUrl.pathname === '' ||
+                  responseUrl.pathname === url ||
+                  responseUrl.origin === url
+              )
+            ) {
+              return resolve(response)
+            }
 
-        const responseToCache = response.clone()
+            const responseToCache = response.clone()
 
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache)
-        })
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseToCache)
+            })
 
-        return response
+            return resolve(response)
+          })
+          .catch((error) => {
+            if (fromCache) {
+              return resolve(fromCache)
+            }
+            throw error
+          })
       })
     })
   )
