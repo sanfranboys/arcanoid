8fbb111a7f99d12f1c4b3b65ff96c69522da7463
Dihlofos <Jeymobile@mail.ru>
2021-03-20 16:14:30 +0300

    Sprint/3/6/ leadaerboard ssr (#51)
    * added ducks for leaderboard
    
    * done leaderboard api
    
    * little refactoring before pr
    
    * change type names
    
    Co-authored-by: Evgeny Samsonov <samsonov@aeroidea.ru>

---
+++ src/ducks/leaderboard/saga.ts
@@ -0,0 +1,39 @@
+import { takeEvery, put, call } from 'redux-saga/effects'
+import { LeadersServices } from 'services'
+import { GetLeaderBoardPayload } from 'services/Leaders/types'
+import { setLeaders } from './actions'
+import { LEADERBOARD_NEW, LEADERBOARD_REQUEST } from './actionTypes'
+import { NewLeaderAction } from './types'
+import { transformLeadersData, transformNewLeaderData } from './utils'
+
+// ВЫНЕСТИ В КОНСТ ПОКА
+const initialReqData: GetLeaderBoardPayload = {
+  ratingFieldName: 'sanfranScore',
+  cursor: 0,
+  limit: 10,
+}
+
+function* sagaWorkerGetLeaders() {
+  try {
+    const res = yield call([LeadersServices, 'getLeaderboard'], initialReqData)
+    yield put(setLeaders(transformLeadersData(res.data)))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function* sagaWorkerNewLeaders({ payload }: NewLeaderAction) {
+  try {
+    yield call(
+      [LeadersServices, 'setNewLeader'],
+      transformNewLeaderData(payload)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default function* sagaWatcher() {
+  yield takeEvery(LEADERBOARD_REQUEST, sagaWorkerGetLeaders)
+  yield takeEvery(LEADERBOARD_NEW, sagaWorkerNewLeaders)
+}
